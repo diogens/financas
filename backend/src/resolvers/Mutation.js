2e87caa8487e95d40f65dd2eb594d90a5ec4eda2
Diogenes <diogensgreen@gmail.com>
2020-03-22 12:09:24 -0300

    GRAPHQL - mutation createAccount

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,7 +1,41 @@
 const bycrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { getUserId } = require("./../utils/utils");
 
 const JWT_SECRET = process.env.JWT_SECRET;
+
+async function createAccount(_, { description }, ctx, info) {
+  const userId = getUserId(ctx);
+  return ctx.db.mutation.createAccount({
+    data: {
+      description,
+      user: {
+        connect: {
+          id: userId
+        }
+      }
+    }
+  });
+}
+
+async function login(_, { email, password }, ctx, info) {
+  const user = await ctx.db.query.user({ where: { email } });
+  if (!user) {
+    throw new Error("Email invalido ou não existe em nosso banco!");
+  }
+
+  const valid = await bycrypt.compare(password, user.password);
+  if (!valid) {
+    throw new Error("Senha errada");
+  }
+
+  const token = jwt.sign({ userId: user.id }, JWT_SECRET, { expiresIn: "2h" });
+  console.log("Passs: ", password);
+  return {
+    token,
+    user
+  };
+}
 
 async function signup(_, args, ctx, info) {
   const password = await bycrypt.hash(args.password, 10);
@@ -18,5 +52,7 @@ async function signup(_, args, ctx, info) {
 }
 
 module.exports = {
-  signup
+  login,
+  signup,
+  createAccount
 };
