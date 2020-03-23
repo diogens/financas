d9c3852d92f8826a8c3808478dfcc16f95e12782
Diogenes <diogensgreen@gmail.com>
2020-03-23 15:31:02 -0300

    Graphql - Mutations: createAccount, createCategory, createRecord

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,10 +1,60 @@
 const bycrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const moment = require('moment')
 const { getUserId } = require("./../utils/utils");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
-async function createAccount(_, { description }, ctx, info) {
+async function createRecord(_, args, ctx, info) {
+  const date = moment(args.date)
+  if(!date.isValid()){
+    throw new Error('Data Invalida!')
+  }
+  const userId = getUserId(ctx);
+  return ctx.db.mutation.createRecord(
+    {
+      data: {
+        user: {
+          connect: { id: userId }
+        },
+        account: {
+          connect: { id: args.accountId }
+        },
+        category: {
+          connect: { id: args.categoryId }
+        },
+        amount: args.amount,
+        type: args.type,
+        date: args.date,
+        description: args.description,
+        tags: args.tags,
+        note: args.note
+      }
+    },
+    info
+  );
+}
+
+async function createCategory(_, { description, operation }, ctx, info) {
+  const userId = getUserId(ctx);
+
+  return ctx.db.mutation.createCategory(
+    {
+      data: {
+        description,
+        operation,
+        user: {
+          connect: {
+            id: userId
+          }
+        }
+      }
+    },
+    info
+  );
+}
+
+function createAccount(_, { description }, ctx, info) {
   const userId = getUserId(ctx);
   return ctx.db.mutation.createAccount({
     data: {
@@ -54,5 +104,7 @@ async function signup(_, args, ctx, info) {
 module.exports = {
   login,
   signup,
-  createAccount
+  createAccount,
+  createCategory,
+  createRecord
 };
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,10 +1,39 @@
 const { getUserId } = require("./../utils/utils");
 
+function categories(_, { operation }, ctx, info) {
+  const userId = getUserId(ctx);
+  let AND = [{ OR: [{ user: { id: userId } }, { user: null }] }];
+
+  AND = !operation ? AND : [...AND, { operation }];
+
+  return ctx.db.query.categories(
+    {
+      where: { AND },
+      orderBy: "description_ASC"
+    },
+    info
+  );
+}
+function accounts(_, args, ctx, info) {
+  const userId = getUserId(ctx);
+  return ctx.db.query.accounts(
+    {
+      where: {
+        OR: [{ user: { id: userId } }, { user: null }]
+      },
+      orderBy: "description_ASC"
+    },
+    info
+  );
+}
+
 function user(_, args, ctx, info) {
   const userId = getUserId(ctx);
   return ctx.db.query.user({ where: { id: userId } }, info);
 }
 
 module.exports = {
+  categories,
+  accounts,
   user
 };
