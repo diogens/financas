927678326d4edabef7d8c8594fa7e538345883b7
Diogenes <diogensgreen@gmail.com>
2020-03-23 19:46:33 -0300

    Query - Records

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,4 +1,44 @@
+const moment = require("moment");
 const { getUserId } = require("./../utils/utils");
+
+function records(_, { month, type, accountsIds, categoriesIds }, ctx, info) {
+  const userId = getUserId(ctx);
+
+  let AND = [{ user: { id: userId } }];
+
+  AND = !type ? AND : [...AND, { type }];
+
+  AND =
+    !accountsIds || accountsIds.length === 0
+      ? AND
+      : [...AND, { OR: accountsIds.map(id => ({ account: { id } })) }];
+
+  AND =
+    !categoriesIds || categoriesIds.length === 0
+      ? AND
+      : [...AND, { OR: categoriesId.map(id => ({ category: { id } })) }];
+
+  if (month) {
+    const date = moment(month, "MM-YYYY");
+    const startDate = date.startOf("month").toISOString();
+    const endDate = date.endOf("month").toISOString();
+
+    AND = [...AND, { date_gte: startDate }, { date_lte: endDate }];
+
+    console.log("Date: ", date);
+    console.log("base Date: ", date.toISOString());
+    console.log("Starte Date: ", startDate);
+    console.log("End Date: ", endDate);
+  }
+
+  return ctx.db.query.records(
+    {
+      where: { AND },
+      orderBy: "date_ASC"
+    },
+    info
+  );
+}
 
 function categories(_, { operation }, ctx, info) {
   const userId = getUserId(ctx);
@@ -35,5 +75,6 @@ function user(_, args, ctx, info) {
 module.exports = {
   categories,
   accounts,
+  records,
   user
 };
